ecb5f57387abc6b5a7e05f5abfbb887fa240a5d8
carlosbarrazamoreno <125162985+carlosbarrazamoreno@users.noreply.github.com>
2023-02-25 00:13:49 -0300

    Add files via upload

---
+++ TICMAS_CV/js/funciones.js
@@ -0,0 +1,9 @@
+
+document.getElementById('boton_descarga').onmouseover = function() {
+    document.getElementById("boton_descarga").style.color = '#ff7613';
+}
+
+
+document.getElementById('boton_descarga').onmouseout = function() {
+    document.getElementById("boton_descarga").style.color = 'white';
+}
